29635de6f4d643a077ed41aee87c611bebce6c47
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-17 21:38:51 +0000

    giraffe and reloader

--- dice.rb
+++ dice.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "sinatra/reloader"
 
 get("/") do
   "Hello World"
@@ -6,4 +7,8 @@ end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
