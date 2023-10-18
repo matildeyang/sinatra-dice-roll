d563a35d789a3017d480e23fd16a21bad82d7324
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-18 01:19:35 +0000

    layout file

--- dice.rb
+++ dice.rb
@@ -30,7 +30,7 @@ get("/dice/2/6") do
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}." 
 
-  erb(:two_six, { :layout => :wrapper})
+  erb(:two_six)
 end 
 
 get("/dice/2/10") do 
@@ -39,17 +39,17 @@ get("/dice/2/10") do
   sum = first_die + second_die
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}." 
-  erb(:two_ten, { :layout => :wrapper})
+  erb(:two_ten)
 end 
 
 get("/dice/1/20") do 
   die_result = rand(1..20)
   @outcome = "You rolled a sum of #{die_result}"
 
-  erb(:one_twenty, { :layout => :wrapper})
+  erb(:one_twenty)
 end
 
-get("/die/5/4") do 
+get("/dice/5/4") do 
   first_die = rand(1..4)
   second_die = rand(1..4)
   third_die = rand(1..4)
@@ -58,5 +58,5 @@ get("/die/5/4") do
 
   @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}." 
 
-  erb(:five_four, { :layout => :wrapper})
+  erb(:five_four)
 end 
